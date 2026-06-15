833db1747f114dac378bd8aa74b25041fbed1b9f
RAJAVARMAN G <rajavarmang1312@gmail.com>
2026-06-15 23:45:58 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,16 @@
+"""
+ My first program.
+ Day: 01
+ Platform: Haker rank
+ Topic: Sum of two num 
+"""
+
+def solveMeFirst(a,b):
+	
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(int(res))
+
